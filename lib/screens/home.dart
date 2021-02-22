d037d1b5d49cbfb6ed69d88d63b43d451d64fb73
amanv2k <amanvermaav2605@gmail.com>
2021-02-22 19:56:52 +0530

    custom nav bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,6 +10,35 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: SnakeNavigationBar.color(
+        // behaviour: snakeBarStyle,
+        // snakeShape: snakeShape,
+        // shape: bottomBarShape,
+        // padding: padding,
+
+        ///configuration for SnakeNavigationBar.color
+        snakeViewColor: Colors.blue,
+        // selectedItemColor: snakeShape == SnakeShape.indicator ? selectedColor : null,
+        unselectedItemColor: Colors.blueGrey,
+
+        ///configuration for SnakeNavigationBar.gradient
+        //snakeViewGradient: selectedGradient,
+        //selectedItemGradient: snakeShape == SnakeShape.indicator ? selectedGradient : null,
+        //unselectedItemGradient: unselectedGradient,
+
+        // showUnselectedLabels: showUnselectedLabels,
+        // showSelectedLabels: showSelectedLabels,
+
+        // currentIndex: _selectedItemPosition,
+        // onTap: (index) => setState(() => _selectedItemPosition = index),
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: 'tickets'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today), label: 'calendar'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+        ],
+      ),
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.blue,
