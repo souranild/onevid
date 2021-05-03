f68020455d0e0a417092dd531c4879fc5fabc8cd
amanv2k <amanvermaav2605@gmail.com>
2021-05-03 22:33:22 +0530

    changed bottom navigation bar (removed the snakeNavigation bar)

--- lib/const.dart
+++ lib/const.dart
@@ -3,3 +3,33 @@ import 'package:flutter/material.dart';
 const bgcolor = Color(0xFF000000);
 const fgcolor = Colors.teal;
 const mgcolor = Color(0xFF292929);
+const iconColor = Color(0xFF607d8b);
+
+class NavBar {
+  const NavBar(this.title, this.icon, this.color);
+  final String title;
+  final IconData icon;
+  final MaterialColor color;
+}
+
+const List<NavBar> navBarIcons = <NavBar>[
+  NavBar('Library', Icons.library_books, Colors.teal),
+  NavBar('Home', Icons.home, Colors.cyan),
+  NavBar('Settings', Icons.settings, Colors.orange),
+];
+
+BottomNavigationBar buildNavBar() {
+  return BottomNavigationBar(
+    // onTap: (int index) {
+    //   setState(() {
+    //     _currentIndex = index;
+    //   });
+    // },
+    items: navBarIcons.map((NavBar navBar) {
+      return BottomNavigationBarItem(
+          icon: Icon(navBar.icon),
+          backgroundColor: navBar.color,
+          title: Text(navBar.title));
+    }).toList(),
+  );
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
+import 'package:onevid/const.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,41 +10,46 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    int _currentIndex;
     return Scaffold(
-      bottomNavigationBar: SnakeNavigationBar.color(
-        // behaviour: snakeBarStyle,
-        // snakeShape: snakeShape,
-        // shape: bottomBarShape,
-        // padding: padding,
-
-        ///configuration for SnakeNavigationBar.color
-        snakeViewColor: Colors.blue,
-        // selectedItemColor: snakeShape == SnakeShape.indicator ? selectedColor : null,
-        unselectedItemColor: Colors.blueGrey,
-
-        ///configuration for SnakeNavigationBar.gradient
-        //snakeViewGradient: selectedGradient,
-        //selectedItemGradient: snakeShape == SnakeShape.indicator ? selectedGradient : null,
-        //unselectedItemGradient: unselectedGradient,
-
-        // showUnselectedLabels: showUnselectedLabels,
-        // showSelectedLabels: showSelectedLabels,
-
-        // currentIndex: _selectedItemPosition,
-        // onTap: (index) => setState(() => _selectedItemPosition = index),
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notifications), label: 'tickets'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today), label: 'calendar'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-        ],
-      ),
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.blue,
         elevation: 0,
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
+              color: Color(0xFF1F1F1F),
+              child: TextField(
+                  decoration: InputDecoration(
+                icon: Icon(
+                  IconData(
+                    0xe5d2,
+                  ),
+                  color: Colors.white,
+                  size: 15,
+                ),
+              )),
+            ),
+          ),
+          Expanded(
+              flex: 5,
+              child: Container(
+                color: bgcolor,
+              )),
+          Expanded(
+              flex: 5,
+              child: Container(
+                color: bgcolor,
+              ))
+        ],
+      ),
+      bottomNavigationBar: buildNavBar(),
     );
   }
 }
