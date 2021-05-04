f08e3140cd4237fb80c0739a3fbe1070069cbb14
souranild <souranil.das@gmail.com>
2021-05-04 23:12:15 +0530

    basic settings.dart without navbar

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:onevid/widgets/navbar.dart';
 
+final List<Widget> settings = [
+  ListTile(
+    leading: Icon(Icons.settings),
+    title: Text('General'),
+    trailing: Icon(Icons.keyboard_arrow_down),
+  ),
+  ListTile(
+    leading: Icon(Icons.list),
+    title: Text('Services'),
+    trailing: Icon(Icons.keyboard_arrow_down),
+  ),
+  ListTile(
+    leading: Icon(Icons.info),
+    title: Text('About'),
+    trailing: Icon(Icons.keyboard_arrow_down),
+  ),
+];
+
 class SettingsPage extends StatefulWidget {
   @override
   _SettingsPageState createState() => _SettingsPageState();
@@ -9,9 +27,17 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      bottomNavigationBar: CustomBottomNavBar(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Container(
+          child: Center(
+            child: ListView(
+              children: settings,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
